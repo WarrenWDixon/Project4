4f796c92a74a0d348b26017173114ceb72dac1ff
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-11-29 21:12:00 -0600

    starting Nov. 29th

---
+++ app/src/main/java/com/udacity/warrenp4/authentication/AuthenticationActivity.kt
@@ -0,0 +1,28 @@
+package com.udacity.warrenp4.authentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.udacity.warrenp4.R
+
+/**
+ * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
+ * signed in users to the RemindersActivity.
+ */
+class AuthenticationActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "LoginFragment"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+    //wwd
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+
+//          TODO: If the user was authenticated, send him to RemindersActivity
+
+//          TODO: a bonus is to customize the sign in flow to look nice using :
+        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+
+    }
+}
