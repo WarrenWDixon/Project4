460da28f672f6b8bc801bf42e79d3194d82b9aeb
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-11-29 21:27:04 -0600

    Nov 29 9:30pm added login logic

--- app/src/main/java/com/udacity/warrenp4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/warrenp4/authentication/AuthenticationActivity.kt
@@ -1,7 +1,9 @@
 package com.udacity.warrenp4.authentication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
 import com.udacity.warrenp4.R
 
 /**
@@ -14,16 +16,34 @@ class AuthenticationActivity : AppCompatActivity() {
         const val SIGN_IN_RESULT_CODE = 1001
     }
     //wwd  2
+    private lateinit var loginBtn : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+        loginBtn = findViewById(R.id.login_button)
+        loginBtn.setOnClickListener {
+            doLogin()
+        }
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    }
 
+    private fun doLogin() {
+        // Give users the option to sign in / register with their email or Google account. If users
+        // choose to register with their email, they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent. We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code.
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers).build(), SIGN_IN_RESULT_CODE)
     }
 }
