784a4f95bb86fc457c698edb9ece58d681fa01d3
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-12-11 20:52:56 -0600

    Dec. 11 9pm add background permissions logic

--- app/src/main/java/com/udacity/warrendproject4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/warrendproject4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,13 +1,19 @@
 package com.udacity.warrendproject4.locationreminders.savereminder
 
+import android.Manifest
+import android.annotation.TargetApi
 import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import com.udacity.warrendproject4.R
@@ -17,11 +23,12 @@ import com.udacity.warrendproject4.databinding.FragmentSaveReminderBinding
 import com.udacity.warrendproject4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.warrendproject4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
-
+const val REQUEST_BACKGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 35
 class SaveReminderFragment : BaseFragment() {
     //Get the view model this time as a single to be shared with the another fragment
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSaveReminderBinding
+    private val runningQOrLater = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +58,7 @@ class SaveReminderFragment : BaseFragment() {
             Log.d("WWD", "isAccepting Text else block")
         }
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -82,7 +90,38 @@ class SaveReminderFragment : BaseFragment() {
 //            TODO: use the user entered reminder details to:
 //             1) add a geofencing request
 //             2) save the reminder to the local db
+            if (!checkBackgroundLocationPermissionApproved()){
+                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q)
+                    requestBackgroundPermission()
+            }
+            else {
+
+            }
+
         }
+    }
+
+    private fun checkBackgroundLocationPermissionApproved() : Boolean {
+        val backgroundPermissionApproved =
+            if (runningQOrLater) {
+                PackageManager.PERMISSION_GRANTED ==
+                        ActivityCompat.checkSelfPermission(
+                            requireActivity(), Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            } else {
+                true
+            }
+        return backgroundPermissionApproved
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun requestBackgroundPermission() {
+        val permissionsArray = arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        val requestCode = REQUEST_BACKGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            permissionsArray,
+            requestCode
+        )
     }
 
     override fun onDestroy() {
