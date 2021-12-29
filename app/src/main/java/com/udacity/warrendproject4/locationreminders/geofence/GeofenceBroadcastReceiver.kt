798a679932ae6cd223b9d0aaafcfa8ed670e92da
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-12-28 19:29:56 -0600

    starting point Dec. 28, first pass broadcast receiver logic

--- app/src/main/java/com/udacity/warrendproject4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/udacity/warrendproject4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -1,8 +1,15 @@
 package com.udacity.warrendproject4.locationreminders.geofence
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingEvent
+import com.udacity.warrendproject4.utils.sendNotification
 
 /**
  * Triggered by the Geofence.  Since we can have many Geofences at once, we pull the request
@@ -18,6 +25,50 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
 
 //TODO: implement the onReceive method to receive the geofencing events at the background
+
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        if (geofencingEvent.hasError()) {
+            val errorMessage = GeofenceStatusCodes
+                .getStatusCodeString(geofencingEvent.errorCode)
+            Log.e("WWD", "error in event $errorMessage")
+            return
+        }
+
+        // Get the transition type.
+        val geofenceTransition = geofencingEvent.geofenceTransition
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+            geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT
+        ) {
+
+            Log.d("WWD", "in enter or exit transition")
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+
+            val fenceId = when {
+                geofencingEvent.triggeringGeofences.isNotEmpty() ->
+                    geofencingEvent.triggeringGeofences[0].requestId
+                else -> {
+                    Log.e("WWD", "No Geofence Trigger Found! Abort mission!")
+                    return
+                }
+            }
+            Log.d("WWD", "fenceId is " + fenceId)
+        } else {
+            // Log the error.
+            Log.e("WWD", "error in broadcast receiver");
+        }
+        /* val notificationManager = ContextCompat.getSystemService(
+             context,
+             NotificationManager::class.java
+         ) as NotificationManager
+
+         notificationManager.sendGeofenceEnteredNotification(
+             context, foundIndex
+         ) */
 
     }
 }
